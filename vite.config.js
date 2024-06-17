7ee2c48aec1074cc3f6fce262e83845f432956b1
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-06-17 22:08:56 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -12,3 +12,5 @@ export default defineConfig({
         }),
     ],
 });
+
+// flex absolute top-0 left-0 flex-col justify-center items-center place-items-center px-5 py-2.5 w-full h-52 text-center rounded-lg border-2 border-dashed opacity-75 transition cursor-pointer dz-message group hover:bg-muted/25 background-muted border-muted-foreground/25 rounded-3 text-muted ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2
