bb68075a58cb4a6d8059f6861671ec910669f186
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-06-19 21:22:41 +0700

    update sidebar admin

--- resources/views/components/navigation/sidebar-admin.blade.php
+++ resources/views/components/navigation/sidebar-admin.blade.php
@@ -1,6 +1,6 @@
-<div class="hidden border-r lg:block">
+<div class="hidden no-underline border-r lg:block">
     <div class="flex flex-col gap-2 h-full max-h-screen">
-        <div class="flex h-[60px] items-center border-b px-6">
+        <div class="flex h-[60px] items-center  px-6">
             <a class="flex gap-2 items-center font-semibold" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -9,7 +9,7 @@
                     <path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.8 1.1L21 9"></path>
                     <path d="M12 3v6"></path>
                 </svg>
-                <span class="">TOKO</span>
+                <span class="">TOKO Luxury</span>
             </a>
             <button
                 class="inline-flex justify-center items-center ml-auto w-8 h-8 text-sm font-medium whitespace-nowrap rounded-md border transition-colors ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground">
@@ -19,7 +19,7 @@
                     <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"></path>
                     <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"></path>
                 </svg>
-                <span class="sr-only">Toggle noti</span>
+                <span class="sr-only">Toggle notif</span>
             </button>
         </div>
         <div class="overflow-auto flex-1 py-2">
@@ -45,7 +45,7 @@
                     </svg>
                     Users
                 </a>
-                <a class="flex gap-3 items-center px-3 py-2  {{request()->is('admin/product') || request()->is('admin/product/create')  ? 'text-accent-foreground' : 'text-muted-foreground hover:text-accent-foreground'}}"
+                <a class="flex gap-3 items-center px-3 py-2  {{request()->is('admin/product') || request()->is('admin/product/create') ? 'text-accent-foreground' : 'text-muted-foreground hover:text-accent-foreground'}}"
                     href="/admin/products">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -69,7 +69,7 @@
                         <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12">
                         </path>
                     </svg>
-                    Order
+                    Orders
                 </a>
                 <a class="flex gap-3 items-center px-3 py-2  {{request()->is('/admin/dashboard') ? '' : 'text-muted-foreground hover:text-accent-foreground'}}"
                     href="#">
@@ -81,7 +81,7 @@
                         </path>
                         <circle cx="12" cy="12" r="3"></circle>
                     </svg>
-                    Setti
+                    Settings
                 </a>
             </nav>
         </div>
