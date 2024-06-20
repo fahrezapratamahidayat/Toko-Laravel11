532e6109e0cdff39f5bcc0050c101fb5480806ca
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-06-20 22:31:50 +0700

    update user-navbar

--- resources/views/components/navigation/user-navbar.blade.php
+++ resources/views/components/navigation/user-navbar.blade.php
@@ -1,17 +1,20 @@
-<nav class="antialiased bg-white dark:bg-gray-800">
+<nav class="antialiased ">
     <div class="px-4 py-4 mx-auto max-w-screen-xl 2xl:px-0">
         <div class="flex justify-between items-center">
 
             <div class="flex items-center space-x-8">
                 <div class="shrink-0">
+
                 </div>
 
                 <ul class="hidden gap-6 justify-start items-center py-3 lg:flex md:gap-8 sm:justify-center">
                     <li>
-                        <a href="/" class="hover:text-primary-700 dark:hover:text-primary-500">Home</a>
+                        <a href="/"
+                            class="hover:text-primary-700 dark:hover:text-primary-500 text-base font-semibold">Home</a>
                     </li>
                     <li>
-                        <a href="/products" class="hover:text-primary-700 dark:hover:text-primary-500">Products</a>
+                        <a href="/products"
+                            class="hover:text-primary-700 dark:hover:text-primary-500 text-base font-semibold">Products</a>
                     </li>
                 </ul>
             </div>
@@ -30,8 +33,7 @@
                         </svg>
                         <span class="hidden sm:flex">My Cart</span>
                         <svg class="hidden w-4 h-4 text-gray-900 sm:flex dark:text-white ms-1" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                            viewBox="0 0 24 24">
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="m19 9-7 7-7-7" />
                         </svg>
@@ -39,10 +41,16 @@
 
                     <div id="myCartDropdown1"
                         class="hidden overflow-hidden z-10 p-4 mx-auto space-y-4 max-w-sm antialiased bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                        <button type="button"
-                            class="px-5 py-2.5 mb-2 w-full text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Checkout Sekarang
-                        </button>
+                        @if (0 > 1)
+                            <button type="button"
+                                class="px-5 py-2.5 mb-2 w-full text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Checkout Sekarang
+                            </button>
+                        @else
+                            <p class="text-sm font-medium text-gray-900 dark:text-white">
+                                Your cart is empty
+                            </p>
+                        @endif
                     </div>
 
                     <button id="userDropdownButton1" data-dropdown-toggle="userDropdown1" type="button"
@@ -54,8 +62,7 @@
                         </svg>
                         Account
                         <svg class="w-4 h-4 text-gray-900 dark:text-white ms-1" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                            viewBox="0 0 24 24">
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="m19 9-7 7-7-7" />
                         </svg>
@@ -64,7 +71,7 @@
                     <div id="userDropdown1"
                         class="hidden overflow-hidden overflow-y-auto z-10 w-56 antialiased bg-white rounded-lg divide-y divide-gray-100 shadow dark:divide-gray-600 dark:bg-gray-700">
                         <ul class="p-2 text-sm font-medium text-gray-900 text-start dark:text-white">
-                            <li><a href="#" title=""
+                            <li><a href="/profile" title=""
                                     class="inline-flex gap-2 items-center px-3 py-2 w-full text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-600">
                                     My Account </a></li>
                             <li><a href="#" title=""
@@ -76,12 +83,6 @@
                             <li><a href="#" title=""
                                     class="inline-flex gap-2 items-center px-3 py-2 w-full text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-600">
                                     Favourites </a></li>
-                            <li><a href="#" title=""
-                                    class="inline-flex gap-2 items-center px-3 py-2 w-full text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-600">
-                                    Delivery Addresses </a></li>
-                            <li><a href="#" title=""
-                                    class="inline-flex gap-2 items-center px-3 py-2 w-full text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-600">
-                                    Billing Data </a></li>
                         </ul>
 
                         <div class="p-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -101,12 +102,12 @@
                         <span class="sr-only">
                             Open Menu
                         </span>
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
                                 d="M5 7h14M5 12h14M5 17h14" />
                         </svg>
-                        /button>
+                    </button>
                 </div>
             @else
                 <button type="button"
@@ -122,10 +123,12 @@
             class="hidden px-4 py-3 mt-4 bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
             <ul class="space-y-3 text-sm font-medium text-gray-900 dark:text-white">
                 <li>
-                    <a href="/" class="hover:text-primary-700 dark:hover:text-primary-500">Home</a>
+                    <a href="/"
+                        class="hover:text-primary-700 dark:hover:text-primary-500 text-base font-semibold">Home</a>
                 </li>
                 <li>
-                    <a href="/products" class="hover:text-primary-700 dark:hover:text-primary-500">Products</a>
+                    <a href="/products"
+                        class="hover:text-primary-700 dark:hover:text-primary-500 text-base font-semibold">Products</a>
                 </li>
                 </li>
             </ul>
@@ -133,7 +136,7 @@
     </div>
 </nav>
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('myCartDropdownButton1').click();
     });
 </script>
