52b0fb736a5588af7421475b0a36a53ea9d9352b
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-06-20 19:36:09 +0700

    update all condisinal rendering

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    @if (auth()->user()->role === 'Admin' || auth()->user()->role === 'Super Admin')
+        <div class="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
+            <x-navigation.sidebar-admin></x-navigation.sidebar-admin>
+            <div class="flex flex-col">
+                <x-navigation.header-admin />
+                <main class="overflow-auto flex-1">
+                    {{ $slot }}
+                </main>
+            </div>
+        </div>
+    @else
+        <div class="flex flex-col min-h-screen w-full">
             <x-navigation.user-navbar />
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow dark:bg-gray-800">
-                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
+            <main class="overflow-auto flex-1">
                 {{ $slot }}
             </main>
         </div>
-    </body>
-    @push('scripts')
-        
-    @endpush
+    @endif
+</body>
+@push('scripts')
+@endpush
+
 </html>
