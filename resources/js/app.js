9df691347d5c87590784b2ae03224e56135a4c29
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-07-11 21:53:41 +0700

    tets

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,16 @@ import './bootstrap';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const modal = new HSOverlay(document.querySelector('#hs-basic-modal-update'));
+const openBtn = document.querySelector('#hs-basic-modal-update');
+
+openBtn.addEventListener('click', () => {
+    modal.open();
+});
+
+// const openBtn = document.querySelector('#open-btn');
+
+openBtn.addEventListener('click', () => {
+    HSOverlay.open('#hs-basic-modal-update');
+});
