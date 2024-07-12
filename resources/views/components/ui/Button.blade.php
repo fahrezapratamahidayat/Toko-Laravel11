25ba34258a5235b46323f145801e07afc73a0984
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-07-12 21:51:02 +0700

    add button component

---
+++ resources/views/components/ui/Button.blade.php
@@ -0,0 +1,4 @@
+<button variant="outline"
+    class="inline-flex justify-center items-center px-4 py-2 h-9 text-sm font-medium whitespace-nowrap rounded-md shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90">
+    {{ $slot ?? 'Default Text' }}
+</button>
