6bcb6a0fdc5980861428e1c36d00318cca3c52f2
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-07-12 21:50:26 +0700

    update register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,52 @@
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
-                autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Username -->
-        <div class="mt-4">
-            <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')"
-                required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex justify-end items-center mt-4">
-            <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="mx-auto max-w-sm rounded-xl border shadow bg-card text-card-foreground">
+            <div class="flex flex-col p-6 space-y-1.5">
+                <h3 class="font-semibold tracking-tight leading-none">Sign Up</h3>
+                <p class="text-sm text-muted-foreground">
+                    Enter your information to create an account
+                </p>
+            </div>
+            <div class="p-6 pt-0">
+                <div class="grid gap-4">
+                    <div class="grid gap-2">
+                        <x-ui.label for="name">Fullname</x-ui.label>
+                        <x-ui.input id="name" name="name" placeholder="Firstname" required />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+                    <div class="grid gap-2">
+                        <x-ui.label for="username">Last name</x-ui.label>
+                        <x-ui.input id="username" name="username" placeholder="Username" required />
+                        <x-input-error :messages="$errors->get('username')" class="mt-2" />
+                    </div>
+                    <div class="grid gap-2">
+                        <x-ui.label for="email">Email</x-ui.label>
+                        <x-ui.input id="email" name="email" placeholder="email" required />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                    <div class="grid gap-2">
+                        <x-ui.label for="password">Password</x-ui.label>
+                        <x-ui.input id="password" name="password" placeholder="password" required />
+                    </div>
+                    <div class="grid gap-2">
+                        <x-ui.label for="password_confirmation">Confirm Password</x-ui.label>
+                        <x-ui.input id="password_confirmation" name="password_confirmation" placeholder="" required />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+                    <button type="submit"
+                        class="inline-flex justify-center items-center px-4 py-2 h-9 text-sm font-medium whitespace-nowrap rounded-md shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90">
+                        Create an account
+                    </button>
+                    <x-ui.Button>Sign with GitHub</x-ui.Button>
+                </div>
+                <div class="mt-4 text-sm text-center">
+                    Already have an account?
+                    <a href="/login" class="underline">
+                        Sign in
+                    </a>
+                </div>
+            </div>
         </div>
     </form>
 </x-guest-layout>
